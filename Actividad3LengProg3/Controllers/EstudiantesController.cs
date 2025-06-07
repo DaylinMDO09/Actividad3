4df4fc11fb7ff5175295ae9139343ca571f08d2d
DaylinMDO09 <daylinmf09@gmail.com>
2025-06-06 22:33:50 -0400

    Create EstudiantesController.cs

---
+++ Actividad3LengProg3/Controllers/EstudiantesController.cs
@@ -0,0 +1,26 @@
+﻿using Actividad3LengProg3.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Actividad3LengProg3.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        private static List<EstudianteViewModel> estudiantes = new List<EstudianteViewModel>();
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(EstudianteViewModel model) 
+        {
+            if (ModelState.IsValid)
+            {
+                estudiantes.Add(model);
+                TempData["Mensaje"] = "Estudiante registrado correctamente.";
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+    }
+}
